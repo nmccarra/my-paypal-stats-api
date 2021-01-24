c4be6e46731aee4cd028b9690f5850706ca08f05
Niall McCarra <niallmccarra@Nialls-MacBook-Pro.local>
2021-01-24 11:11:30 +0000

    master: remove blank lines

--- src/main/kotlin/org/nmccarra1/my/paypal/stats/api/controllers/InfoController.kt
+++ src/main/kotlin/org/nmccarra1/my/paypal/stats/api/controllers/InfoController.kt
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class InfoController {
-
     @GetMapping("/info")
     fun info() = "My Paypal Stats API v0.1"
-    
 }
