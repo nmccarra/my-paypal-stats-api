b5278b92b2cd61f1a059e9b964105b2c1aa3a8af
Niall McCarra <niallmccarra@Nialls-MacBook-Pro.local>
2021-01-24 10:55:57 +0000

    add-info: add info endpoint

---
+++ src/main/kotlin/org/nmccarra1/my/paypal/stats/api/controllers/InfoController.kt
@@ -0,0 +1,12 @@
+package org.nmccarra1.my.paypal.stats.api.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class InfoController {
+
+    @GetMapping("/info")
+    fun info() = "My Paypal Stats API v0.1"
+    
+}
